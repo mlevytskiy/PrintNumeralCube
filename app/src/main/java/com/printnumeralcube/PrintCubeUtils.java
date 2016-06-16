46651f3d38578eafe8f22ec54d5f1a0fcbde365b
Maksym Levytskyi <maksym.levytskyi@mantu.im>
2016-06-16 19:56:31 +0300

    reuse strBuilder

--- app/src/main/java/com/printnumeralcube/PrintCubeUtils.java
+++ app/src/main/java/com/printnumeralcube/PrintCubeUtils.java
@@ -14,18 +14,19 @@ public class PrintCubeUtils {
         if (n <= 0) {
             throw new IllegalArgumentException("Incorrect n = " + n);
         }
+        StringBuilder strBuilder = new StringBuilder();
         Integer[] biggestLine = getBiggestLine(n);
         for (int i = 1; i < n; i++) {
-            println( getLine(biggestLine, i, n) );
+            println( getLine(biggestLine, i, n, strBuilder) );
         }
         println( TextUtils.join("", biggestLine) );
         for (int i = n-1; i > 0; i--) {
-            println( getLine(biggestLine, i, n) );
+            println( getLine(biggestLine, i, n, strBuilder) );
         }
     }
 
-    private static String getLine(Integer[] biggestLine, int lineNumber, int maxValueInCube) {
-        StringBuilder strBuilder = new StringBuilder();
+    private static String getLine(Integer[] biggestLine, int lineNumber, int maxValueInCube, StringBuilder strBuilder) {
+        strBuilder.setLength(0); //prepare strBuilder for reuse
         for (int i = 0; i < biggestLine.length; i++) {
             int value = biggestLine[i] - (maxValueInCube - lineNumber);
             strBuilder.append( value > 0 ? value : " " );
